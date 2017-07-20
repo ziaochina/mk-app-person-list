aa7f3dc248d2a2d06fe4b5fa2830e5286d36c152
ziaochina <ziaochina@gmail.com>
2017-07-20 09:30:06 +0800

    zlj

---
+++ example/index.js
@@ -0,0 +1,21 @@
+import { config, start, componentFactory } from 'mk-meta-engine'
+import myConfig  from './config'
+
+import mk_app_person_list from './apps/mk-app-person-list/index.js'
+
+const apps = {
+	[mk_app_person_list.name]:mk_app_person_list,	
+}
+
+
+config(myConfig({apps}))
+
+
+import * as mkComponents from 'mk-component'
+
+Object.keys(mkComponents).forEach(key=>{
+	componentFactory.registerComponent(key, mkComponents[key])
+})
+	
+
+start()
---
+++ example/mock.js
@@ -0,0 +1,5 @@
+import { fetch } from 'mk-utils'
+
+fetch.config({mock:true})
+
+import "./apps/mk-app-person-list/mock.js"
