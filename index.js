d27b41d8611308b861c709ff2f18cdf04206e6d3
ziaochina <ziaochina@gmail.com>
2017-07-21 09:45:48 +0800

    zlj

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import * as data from './data'
 
 export default {
 	name: "mk-app-person-list",
-	version: "1.0.0",
+	version: "1.0.3",
 	description: "mk-app-person-list",
 	meta: data.getMeta(),
 	components: [],
